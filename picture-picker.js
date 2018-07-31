6ae220f42886bdf6c7bf1db04c527a66a6c1a8c7
lucaspouchot <lucas.pouchot@digitalairways.com>
2018-07-31 18:13:52 +0200

    fix display picture + load pictures from /root/userdir/images

--- picture-picker.js
+++ picture-picker.js
@@ -1,32 +1,83 @@
 module.exports = function ( RED ) {
   "use strict";
   var fs = require("fs");
-  function main ( config ) {
-    RED.nodes.createNode( this, config );
-    this.picture = config.picture;
+  var path = require("path");
+
+  const imagesDir = "/root/userdir/images"
+  const imageMimeTypes = [
+    {
+      mime: 'image/jpeg',
+      exts: ['.jpeg', '.jpg']
+    },
+    {
+      mime: 'image/png',
+      exts: ['.png']
+    },
+    {
+      mime: 'image/gif',
+      exts: ['.gif']
+    },
+    {
+      mime: 'image/bmp',
+      exts: ['.bmp']
+    },
+    {
+      mime: 'image/webp',
+      exts: ['.webp']
+    }
+  ]
+
+  function main(n){
+    RED.nodes.createNode(this, n);
+    this.picture = n.picture;
+    this.picture_name = n.picture_name;
     var node = this;
     this.on("input", function(msg) {
       var picture = node.picture;
-      if(typeof(msg.picture) != "undefined" && msg.picture != ""){picture = msg.picture;}
-      if(picture != ""){
-        msg.picture = picture;
+      if(typeof msg.picture !== "undefined" && msg.picture !== ""){
+        picture = msg.picture;
+      }
+      if(picture !== ""){
+        msg.picture = path.resolve(imagesDir, picture);
       }
       node.send(msg);
     });
   }
   RED.nodes.registerType("picturepicker", main);
 
+  function getPicture(p){
+    return (new Promise(resolve => {
+      fs.readFile(p, (err, data) => {
+        if(err) { resolve(null) }
+        else {
+          let pathInfo = path.parse(p)
+          let mime = imageMimeTypes.filter( i => i.exts.indexOf(pathInfo.ext.toLowerCase()) !== -1)
+          if(mime.length < 1){
+            resolve(null)
+          } else {
+            resolve({
+              data: data.toString('base64'),
+              path: p,
+              name: pathInfo.name,
+              mime_type: mime[0].mime
+            })
+          }
+        }
+      });
+    }))
+  }
+
   RED.httpAdmin.get("/picture/list", function(req, res) {
-    fs.readdir(__dirname + "/icons/",function(err, files){
-      var tabPics = [];
-      var i = 0;
-      for(var f in files){
-        tabPics[i] = [];
-        tabPics[i].push("/icons/" + files[i]);
-        tabPics[i].push(__dirname + "/icons/" + files[i]);
-        i++;
+    fs.readdir(imagesDir, function(err, filesImages){
+      var promisesImages = []
+      for(var f in filesImages){
+        promisesImages.push(getPicture(path.join(imagesDir, filesImages[f])))
       }
-      res.send(tabPics);
+
+      Promise.all(promisesImages).then( dataImages => {
+        dataImages = dataImages.filter(d => d)
+        res.send(dataImages)
+      })
     });
   });
 }
